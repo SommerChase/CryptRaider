9d0553fbb99d015ae982d734734776fb6764be18
Chase <robertchasesommer@gmail.com>
2023-10-16 17:26:55 -0400

    Lecture 100 - Constructors

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -3,10 +3,22 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+	// This is added by default for Actors, but not components? I manually added this
+	// to enable ticking and viewing logs for each tick.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
 // Called when the game starts
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-	UE_LOG(LogTemp, Display, TEXT("I'm triggered bb"))
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UE_LOG(LogTemp, Display, TEXT("I'm a ticking timebomb!"))
 }
